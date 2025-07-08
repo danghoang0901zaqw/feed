af1421d343ded9a627b398f343a64e06e0bd03fc
hoang tran <hoang.tran@apps-cyclone.com>
2025-07-08 11:24:15 +0700

    chore(setup): initialize project structure

---
+++ src/models/database/User.schema.ts
@@ -0,0 +1,62 @@
+import { ObjectId } from 'mongodb'
+
+enum UserVerifyStatus {
+  Unverified,
+  Verified,
+  Banned
+}
+interface UserI {
+  _id: ObjectId
+  name: string
+  email: string
+  date_of_birth: Date
+  password: string
+  created_at: Date
+  updated_at: Date
+  email_verify_token: string | null
+  forgot_password_token: string | null
+  verify: UserVerifyStatus
+  bio: string | null
+  location: string | null
+  website: string | null
+  username: string
+  avatar: string | null
+  cover: string | null
+}
+export default class User {
+  _id: ObjectId
+  name: string
+  email: string
+  date_of_birth: Date
+  password: string
+  created_at: Date
+  updated_at: Date
+  email_verify_token: string | null
+  forgot_password_token: string | null
+  verify: UserVerifyStatus
+  bio: string | null
+  location: string | null
+  website: string | null
+  username: string
+  avatar: string | null
+  cover: string | null
+  constructor(user: UserI) {
+    this._id = user._id
+    this.name = user.name
+    this.email = user.email
+    this.date_of_birth = user.date_of_birth
+    this.password = user.password
+    this.created_at = user.created_at
+    this.updated_at = user.updated_at
+    this.email_verify_token = user.email_verify_token
+    this.forgot_password_token = user.forgot_password_token
+    this.verify = user.verify
+    this.bio = user.bio
+    this.location = user.location
+    this.website = user.website
+    this.username = user.username
+    this.avatar = user.avatar
+    this.cover = user.cover
+  }
+
+}
---
+++ src/services/database.services.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+import { Collection, Db, MongoClient, ServerApiVersion } from 'mongodb'
+import User from '~/models/database/User.schema'
+dotenv.config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gq6vrxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+class DatabaseService {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+      }
+    })
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+  async connect() {
+    try {
+      await this.client.connect()
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      await this.client.close()
+    }
+  }
+  get users(): Collection<User> {
+    return this.db.collection('users')
+  }
+}
+export default new DatabaseService()
