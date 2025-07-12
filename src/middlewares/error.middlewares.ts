c7ec9abe661716792a81536618a2b8096070a978
hoang tran <hoang.tran@apps-cyclone.com>
2025-07-12 09:27:35 +0700

    fix(error): fix bug status error

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -10,7 +10,7 @@ export const errorMiddlewares = (error: Error, req: Request, res: Response, next
   const customError = error as CustomError
   const status =
     customError?.status && String(customError.status).startsWith('4')
-      ? HTTP_STATUS.BAD_REQUEST
+      ? customError.status
       : HTTP_STATUS.INTERNAL_SERVER_ERROR
   const statusCode = customError.statusCode || 'error'
   const message = customError.message || 'Internal Server Error'
