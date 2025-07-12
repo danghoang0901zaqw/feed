f0b3f5a8307da58c79a625cd1302b3f38c8950b8
hoang tran <hoang.tran@apps-cyclone.com>
2025-07-12 09:44:10 +0700

    feat(route-not-match): handle error route not match

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,14 @@
 import { Express } from 'express'
 import userRouter from './users.routes'
+import AppError from '~/controllers/error.controler'
+import HTTP_STATUS from '~/constants/httpStatus'
 const route = (app: Express) => {
   app.use('/v1/users/', userRouter)
+  app.all(/(.*)/, (req, res, next) => {
+    const original = req.originalUrl
+    const path = req.path
+    next(new AppError(`Cannot find ${original} ${path} on this server`, HTTP_STATUS.NOT_FOUND))
+  })
 }
 
 export default route
